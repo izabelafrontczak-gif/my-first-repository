dbb69c2eb1a74cf0bcf9fc3e83da4eace7d724b3
Izabela Frontczak <izabelafrontczak@Izabelas-MacBook-Air.local>
2026-03-24 11:18:47 +0100

    Solve task 3

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-
-if number > 0:
-    print(f"{number} is positive")
-elif number == 0:
-    print(f"{number} is zero")
-else:
-    print(f"{number} is negative")
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
 
